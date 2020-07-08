f0440b274e6b0c5f2abe2d3f295cc956f2aa4471
Hubz <33783503+Hubza@users.noreply.github.com>
2020-07-08 06:47:54 +0100

    removed data for subete, subetepl, survival, anarchy. moved to SubeteMain

--- src/main/java/uk/co/hubza/subetelobby/subetelobby.java
+++ src/main/java/uk/co/hubza/subetelobby/subetelobby.java
@@ -22,30 +22,12 @@ public final class subetelobby extends JavaPlugin {
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    	if (cmd.getName().equalsIgnoreCase("test")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
+    	if (cmd.getName().equalsIgnoreCase("test")) {
     		sender.sendMessage("Hey there, you found our secret command! Welp, here we go!");
     		sender.sendMessage("You, " + sender.getName() + ", have just ran the command /" + cmd.getName() + " : " + cmd);
     		return true;
     	} //If this has happened the function will return true. 
-    	else if (cmd.getName().equalsIgnoreCase("subete")) {
-    		sender.sendMessage(ChatColor.BLUE + "Thanks for playing on Subete! We hope you enjoy the server! Use /help SubeteLobby");
-    		return true;
-    	}
-    	else if (cmd.getName().equalsIgnoreCase("subetepl")) {
-    		sender.sendMessage(ChatColor.BLUE + "This server uses many plugins created by the spigot and bukkit community (see /pl) and also uses many plugins created by the Subete team. Complete credits will be available at subete.hubza.co.uk/pl but at the moment are not.");
-    		return true;
-    	}
-    	else if (cmd.getName().equalsIgnoreCase("survival")) {
-    		sender.sendMessage(ChatColor.BLUE + "Sending you to the Survival server!");
-    		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "server survival");
-    		return true;
-    	}
-    	else if (cmd.getName().equalsIgnoreCase("anarchy")) {
-    		sender.sendMessage(ChatColor.BLUE + "Sending you to the Anarchy server!");
-    		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "server anarchy");
-    		return true;
-    	}
-            // If this hasn't happened the value of false will be returned.
+        
     	return false; 
     }
 }
